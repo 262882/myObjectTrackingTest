f8e35ab3259d13c7420791931680f5f6522973e3
262882 <2628829@students.wits.ac.za>
2023-01-31 16:01:01 +0200

    Added multi object simulation

--- run_sim.py
+++ run_sim.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
+
 import world_model
 import numpy as np
 import cv2
 
 if __name__ == '__main__':
-	print("Welcome to the Object Tracking Test creator")
+	print("Welcome to the Object Tracking Test Generator")
 
 	print("Initialising simulator")
-	test_sim = world_model.NewWorld('./image/crop_lenna.png')
+	objects = [['./image/sunflower.tif',[-2,2]],
+				['./image/hill.jpg',[2,-2]],
+				['./image/parrot.jpg',[-2,-2]],
+				['./image/crop_lenna.png',[2,2]]]
 
+	test_sim = world_model.NewWorld(objects, obj_size=81, height=480, width=640)
+
+	# Set capture parameters
 	print("Start recording")
-	FPS = 30
-	duration = 10 #seconds
+	FPS = 30 # Capture rate
+	duration = 10 # seconds
 	video = cv2.VideoWriter('./test_result.avi', cv2.VideoWriter_fourcc(*'MP42'), float(FPS), (test_sim.canvas.shape[1], test_sim.canvas.shape[0]))
 
 	# Main loop
 	for frame in range(FPS*duration):
 		print("Process frame: " + str(frame))
-		cv2.imwrite("test1.jpg", test_sim.canvas)
-		
-		# Add graphics
-		#canvas = cv2.putText(canvas, str(myObj.bounce), (myObj.pos[1],myObj.pos[0]), font, 1, color)
 		video.write(test_sim.canvas)
 		test_sim.step()
 		
--- world_model.py
+++ world_model.py
@@ -9,39 +9,44 @@ class NewWorld:
     A class that represents a world space
 
     Keyword arguments:
-    size -- the width and height of the object (default 41)
-    pos -- the position of the center of the object (default [0, 0])
-    velo -- the velocity of the object (default [0, 0])
-    img -- the path to the image
+    def_obj -- A list of objects of the form [path,[vertical velocity, horizontal velocity]]
+    obj_size -- the size of the objects
+    height -- the height of the map (default 240)
+    width -- the width of the map (default 360)
+    num_obstacles -- the number of obstuctions generated (default 5)
+    thickness_wall -- the thickness of the obstructuions (default 4)
     """
 
-    def __init__(self, img, size=41, pos=[0, 0], velo=[0, 0]):
+    def __init__(self, def_obj, obj_size=41, height=240, width=360, num_obstacles=5, thickness_wall=4):
 
-        self.sum_empty = 3*255*(size)**2
-        self.sum_edge = 3*255*(size)          # Count edge of object, ignore corners
+        # Evaluate checksums 
+        self.sum_empty = 3*255*(obj_size)**2
+        self.sum_edge = 3*255*(obj_size)          # Count edge of object, ignore corners
+
+        # Load objects
         self.object_list = []
-        self.add_object(img)
+        for entry in def_obj:
+            self.__add_object(entry[0],obj_size,[height//2,width//2], entry[1])
 
         # Generate until valid map (No initial clash)
-        self.myMaze = maze_gen.NewMap()
+        self.myMaze = maze_gen.NewMap(m=height, n=width, n_obstacles=num_obstacles, t_wall=thickness_wall)
         top_left = [self.object_list[0].pos[0]-self.object_list[0].size//2-1, self.object_list[0].pos[1]-self.object_list[0].size//2-1]
         curr_occupancy = self.myMaze.map[top_left[0]:top_left[0]+self.object_list[0].size, top_left[1]:top_left[1]+self.object_list[0].size]
         while np.sum(curr_occupancy) != self.sum_empty:
-            self.myMaze = maze_gen.NewMap()
+            self.myMaze = maze_gen.NewMap(m=height, n=width, n_obstacles=num_obstacles, t_wall=thickness_wall)
             curr_occupancy = self.myMaze.map[top_left[0]:top_left[0]+self.object_list[0].size, top_left[1]:top_left[1]+self.object_list[0].size]
-
         self.canvas = self.myMaze.map
 
-    def add_object(self,path,vel=[2,2]):
-        self.object_list.append(track_object.NewObject(path))
-        self.object_list[-1].pos = [self.object_list[-1].size*2,self.object_list[-1].size*2]
+    def __add_object(self,path,size=41, pos=[0,0],vel=[0,0]):
+        self.object_list.append(track_object.NewObject(path, size, pos, vel))
+        self.object_list[-1].pos = pos
         self.object_list[-1].velo = vel
 
-    def update_pos(self):
+    def __update_pos(self):
         for obj in self.object_list:
             obj.pos = [obj.pos[0] + obj.velo[0],obj.pos[1] + obj.velo[1]]
     
-    def update_collisions(self):
+    def __update_collisions(self):
         for obj in self.object_list:
             # Bottom
             if np.sum(self.myMaze.map[obj.pos[0]+obj.size//2+obj.velo[0], obj.pos[1]-obj.size//2-1:obj.pos[1]+obj.size//2]) < self.sum_edge:
@@ -56,13 +61,13 @@ class NewWorld:
             if np.sum(self.myMaze.map[obj.pos[0]-obj.size//2-1:obj.pos[0]+obj.size//2, obj.pos[1]-obj.size//2-1+obj.velo[1]]) < self.sum_edge:
                 obj.velo[1] = -obj.velo[1]
 
-    def update_world(self):
+    def __update_world(self):
         self.canvas = np.copy(self.myMaze.map)  # Fetch blank map
         for obj in self.object_list:
             self.canvas[obj.pos[0]-obj.size//2-1:obj.pos[0]+obj.size//2, obj.pos[1]-obj.size//2-1:obj.pos[1]+obj.size//2] = obj.img
 
     def step(self):
-        self.update_pos()
-        self.update_collisions()
-        self.update_world()
+        self.__update_pos()
+        self.__update_collisions()
+        self.__update_world()
 
